360c5e70103e89c210aee179a777a317fe0c6bba
bsjsmabsdksb <89156786+bsjsmabsdksb@users.noreply.github.com>
2021-08-24 06:20:48 +0800

    Update welcome.js

--- Episode 14/commands/Mod/welcome.js
+++ Episode 14/commands/Mod/welcome.js
@@ -1,11 +1,11 @@
 const { MessageEmbed } = require("discord.js")
 
-module.exports = (client) => {
-    const welcomechannelId = '788270829581762580' //Channel You Want to Send The Welcome Message
-    const targetChannelId = `788271045660246077` //Channel For Rules
+module.exports = 879489964860133506 => {
+    const welcomechannelId = '877430664591446117' 
+    const targetChannelId = `877428225402347560` 
 
     client.on('guildMemberAdd', (member) => {
-        console.log(member) // If You Want The User Info in Console Who Joined Server Then You Can Add This Line. // Optional
+        console.log(member) 
         const channel = member.guild.channels.cache.get(welcomechannelId)
 
         const embed = new MessageEmbed()
@@ -14,21 +14,10 @@ module.exports = (client) => {
         .setDescription(`Hello <@${member.user.id}>, Welcome to **${member.guild.name}**. Thanks For Joining Our Server.
 Please Read ${member.guild.channels.cache.get(targetChannelId).toString()}. Have a Nice Time.
 Chat In <#787083837833871404>`)
-        // You Can Add More Fields If You Want
+        
         .setFooter(`Welcome ${member.user.username}#${member.user.discriminator}`,member.user.displayAvatarURL({dynamic: true, size: 512}))
         .setColor('RANDOM')
     channel.send(embed)
         
-    })
-
-
-    const leavechannelId = '788272431868936194' //Channel You Want to Send The Leave Message
-
-    client.on('guildMemberRemove', (member) => {
-        // You Can Do The Same For Leave Message
-        const leavemessage = `<@${member.id}> Just Left Server.`
-
-        const channel1 = member.guild.channels.cache.get(leavechannelId)
-        channel1.send(leavemessage)
     })
 }
